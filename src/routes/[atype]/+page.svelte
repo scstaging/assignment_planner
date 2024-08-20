52f851429e4fa0599864acb888af29abc2b3cea3
scstaging <scstaging@hotmail.com>
2024-08-19 22:38:57 -0400

    Intro blurb distinct text

--- src/routes/[atype]/+page.svelte
+++ src/routes/[atype]/+page.svelte
@@ -85,6 +85,7 @@
         introBlurbContent = line.slice(1).trim();
         introBlurbContent = introBlurbContent.replace(/\+/g, '<br>');
         introBlurbContent = introBlurbContent.replace(/{([^}]+)}/g, '<a href="$1">$1</a>');
+        introBlurbContent = introBlurbContent.replace(/\[([^\]]+)\]/g, '<p class="distinct-para">$1</p>');
       } else if (line.startsWith('#')) {
         if (currentGoal) {
           goals.push(currentGoal);
