b467da3a7f40e7553a057227fddfe356b3ecbe80
scstaging <scstaging@hotmail.com>
2024-08-28 20:40:05 -0400

    ...

--- src/routes/[atype]/+page.svelte
+++ src/routes/[atype]/+page.svelte
@@ -405,24 +405,14 @@
                     <h2 style="margin-bottom:20px;" class="gp-title-text">{atype}</h2>
                 </div>
                 <div style="display: flex;flex-direction:row;">
-                    {#if selectedGoal === null}
-                        <p class="gp-p-text">Due Date: &nbsp;</p>
-                        <p style="color: rgba(255,85,0,1);" class="gp-p-text">{formattedEndDate}</p>
-                    {:else}
-                        {#each goals as goal}
-                            {#if selectedGoal?.id === goal.id}
-                                <p class="gp-p-text">By&nbsp;</p>
-                                <p style="color: rgba(255,85,0,1);" class="gp-p-text">{goal.dueDate}</p>
-                                <p class="gp-p-text">&nbsp;you should...</p>
-                            {/if}
-                        {/each}
-                    {/if}
+                    <p class="gp-p-text">Due Date: &nbsp;</p>
+                    <p style="color: rgba(255,85,0,1);" class="gp-p-text">{formattedEndDate}</p>
                 </div>
             </div>
 
             <div class="goal-list" transition:fade>
                 {#each goals as goal}
-                    <div style="box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;margin-bottom:15px;" class="gp-goal" on:click={() => selectGoal(goal)}>
+                    <div style="{selectedGoal?.id === goal.id ? "background-color: rgb(145, 35, 56, 0.36)" : "white"};box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;margin-bottom:15px;" class="gp-goal" on:click={() => selectGoal(goal)}>
                         <input id={goal.id} type="checkbox" class="checkbox"
                         on:click={(e) => e.stopPropagation()}
                         on:change={(e) => {
@@ -436,13 +426,13 @@
                         </div>
                     </div>
                     {#if selectedGoal?.id === goal.id}
-                    <div style="display: flex;flex-direction:column;">
-                        <p class="gp-descript" out:fade in:fade={{
+                    <div transition:fade style="display: flex;flex-direction:column;">
+                        <p style="margin-bottom:20px;" class="gp-descript" out:fade in:fade={{
                             delay: 1000
                         }}>{goal.goalDescript}</p>
                         <h2 class="gp-goals-title" transition:fade>Helpful Links</h2>
                         {#each goal.links as link}
-                            <div style="margin-bottom: 20px;margin-top:40px;">
+                            <div style="margin-top:40px;">
                                 <a style="text-decoration: none;" target="_blank" class="link-descript" href={link.descript}><h2 class="link-title">{link.title}</h2></a>
                             </div>
                         {/each}
