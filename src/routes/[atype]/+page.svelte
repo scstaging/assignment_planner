1c2374108f4d9872d85b18036514def8fa39a6a3
scstaging <scstaging@hotmail.com>
2024-08-28 20:49:12 -0400

    ...

--- src/routes/[atype]/+page.svelte
+++ src/routes/[atype]/+page.svelte
@@ -347,10 +347,9 @@
             {/each}
         <div class="goal-list-container" transition:fade>
             <h2 class="gp-goals-title" transition:fade>{selectedGoal === null ? "Plan" : "Helpful Links"}</h2>
-            {#if selectedGoal === null}
             <div class="goal-list" transition:fade>
                 {#each goals as goal}
-                    <div style="box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;margin-bottom:15px;" class="gp-goal" on:click={() => selectGoal(goal)}>
+                    <div style="{selectedGoal?.id === goal.id ? "background-color: rgb(145, 35, 56, 0.36)" : "white"};box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;margin-bottom:15px;" class="gp-goal" on:click={() => selectGoal(goal)}>
                         <input id={goal.id} type="checkbox" class="checkbox"
                         on:click={(e) => e.stopPropagation()}
                         on:change={(e) => {
@@ -363,19 +362,21 @@
                             <h2>By {goal.dueDate} you should: {goal.title}</h2>
                         </div>
                     </div>
+                    {#if selectedGoal?.id === goal.id}
+                    <div transition:fade style="display: flex;flex-direction:column;margin-bottom:40px;">
+                        <p style="margin-bottom:20px;" class="gp-descript" out:fade in:fade={{
+                            delay: 1000
+                        }}>{goal.goalDescript}</p>
+                        <h2 class="gp-goals-title" transition:fade>Helpful Links</h2>
+                        {#each goal.links as link}
+                            <div style="margin-top:40px;">
+                                <a style="text-decoration: none;" target="_blank" class="link-descript" href={link.descript}><h2 class="link-title">{link.title}</h2></a>
+                            </div>
+                        {/each}
+                    </div>
+                    {/if}
                 {/each}
             </div>
-            {/if}
-            {#each goals as goal}
-                {#if selectedGoal?.id === goal.id}
-                    {#each goal.links as link}
-                        <div style="margin-bottom: 20px;margin-top:40px;">
-                            <a style="text-decoration: none;" target="_blank" class="link-descript" href={link.descript}><h2 class="link-title">{link.title}</h2></a>
-                        </div>
-                    {/each}
-                    <h2 class="back-button" on:click={() => {nullSelectedGoal(goal)}}>Back</h2>
-                {/if}
-            {/each}
         </div>
         <div style="width: 100%;display: flex;flex-direction:row;align-items:center;justify-content:space-between;margin-top:40px;">
             {#each goals as goal}
