5dd501fad9dcc0f2dbfa7289dc5de21c7a2f8510
scstaging <scstaging@hotmail.com>
2024-09-16 16:33:10 -0400

    ...

--- src/routes/[atype]/+page.svelte
+++ src/routes/[atype]/+page.svelte
@@ -332,6 +332,8 @@ const accessibilitySelectGoal = (goal) => {
     // Set focus to selected goal for screen reader
     goalRefs[goal.id].focus();
 
+    console.log(goalRefs[goal.id])
+
     // Speak Goal
     let synth = new SpeechSynthesisUtterance("" + goalRefs[goal.id].title + ". " + goalRefs[goal.id].goalDescript);
             
