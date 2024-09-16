bb8fe67a5d8d912037ed5bba33b0203619fbd393
scstaging <scstaging@hotmail.com>
2024-09-16 16:28:11 -0400

    ...

--- src/routes/[atype]/+page.svelte
+++ src/routes/[atype]/+page.svelte
@@ -333,7 +333,7 @@ const accessibilitySelectGoal = (goal) => {
     goalRefs[goal.id].focus();
 
     // Speak Goal
-    let synth = new SpeechSynthesisUtterance(selectedGoal[goal.id].title + ". " + selectedGoal[goal.id].descript);
+    let synth = new SpeechSynthesisUtterance(selectedGoal[goal.id].title + ". " + selectedGoal[goal.id].goalDescript);
             
     // Select a voice
     const voices = speechSynthesis.getVoices();
