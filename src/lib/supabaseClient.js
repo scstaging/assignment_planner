ac1dc05bf397d6bc4c281360183a877fbc55262d
scstaging <scstaging@hotmail.com>
2024-10-08 12:46:06 -0400

    ...

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2am9namFpaXVxc2tscWt3bHJqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTU5NDAwOSwiZXhwIjoyMDM3MTcwMDA5fQ.-lW71rH-yNBpMdWOk8DqPLOd7ZLPpsAPjrZEYikO6OE';
+const supabaseUrl = 'https://vvjogjaiiuqsklqkwlrj.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2am9namFpaXVxc2tscWt3bHJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE1OTQwMDksImV4cCI6MjAzNzE3MDAwOX0.xUVfiRzJerwYP25B6q780lvfDKV4r2mOCg7iwe-I39M';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
