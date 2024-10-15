32e96277d82c32e5c2dd0387016bcf34f47bb7c1
scstaging <scstaging@hotmail.com>
2024-10-15 14:50:03 -0400

    ...

--- src/routes/[atype]/+page.svelte
+++ src/routes/[atype]/+page.svelte
@@ -443,8 +443,11 @@ onMount(() => {
 
     function openFirstGoal()
     {
-        console.log("intersecting")
-        selectedGoal = goals[0];
+        if (introBlurb)
+        {
+            console.log("intersecting")
+            selectedGoal = goals[0];   
+        }
     }
     //*********** END: OBSERVE FOR SCROLL ***********//
 
@@ -480,7 +483,6 @@ onMount(() => {
             <div class="goal-list-container" transition:fade>
               <h2 class="gp-goals-title" transition:fade>Plan</h2>
               <IntersectionObserver
-              once
               element={observableElem}
               bind:intersecting>
               <div bind:this={observableElem} class="goal-list" transition:fade>
