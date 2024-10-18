66207ec598012d4f49566ee1fd4e6b258c47507f
scstaging <scstaging@hotmail.com>
2024-10-18 16:26:12 -0400

    ...

--- src/routes/[atype]/+page.svelte
+++ src/routes/[atype]/+page.svelte
@@ -538,10 +538,8 @@ onMount(() => {
                                 <h2 style="font-size:1.6vw;">Step {goal.id+2}: {goal.title}</h2>
                             </div>
                             {#if goal.title != "Review your graded work" && goal.title != "Get speaking"}
-                            <div style="width: 50%;display:flex;justify-content:flex-end;margin-right:40px;">
-                                <div style="display: flex;flex-direction:row;justify-content:flex-start;">
-                                    <h2 style="font-size:1.6vw;">Complete by {goal.dueDate}</h2>
-                                </div>
+                            <div style="width: 50%;display:flex;justify-content:flex-start;margin-right:40px;">
+                                <h2 style="font-size:1.6vw;">Complete by {goal.dueDate}</h2>
                             </div>
                             {/if}
                         </div>
